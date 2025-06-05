8519942bfdaef083fe330b1cc64edfe1f919bf82
Alessandro Scipioni <scipioale1@gmail.com>
2025-06-06 00:02:40 +0200

    Add files via upload
    Menù di base per eseguire comandi ta tastiera

---
+++ Operazioni_da_seriale.ino
@@ -0,0 +1,38 @@
+void setup() {
+  Serial.begin(500000);
+  while (!Serial);  
+}
+
+void readFile(const char* fileName) {
+  Serial.println("Error opening file.");
+
+}
+
+void readCommand(String &command) {
+  if (Serial.available() > 0) {
+    command = Serial.readStringUntil('\n');
+    command.trim();  // remove trailing newline and spaces
+  }
+}
+
+void showMenu(String &command, const char* filename) {
+  Serial.println("Select mode:");
+  Serial.println(" A Read-filename");
+
+  while (command == "") {
+    readCommand(command);
+  }
+
+  if (command == "A") {
+    readFile(filename);
+  } else {
+    Serial.println("Invalid command.");
+  }
+}
+
+void loop() {
+  const char* filename = "DATALOG.txt";
+  String command = "";
+
+  showMenu(command, filename);
+}
